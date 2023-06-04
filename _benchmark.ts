ee8fb4684230ded2a459672728851eafdc85047d
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-06-04 20:41:41 +0800

    refactor: improve benchmark code organization and docs

--- _benchmark.ts
+++ _benchmark.ts
@@ -1,7 +1,6 @@
 import pSeries from "npm:p-series";
 import {
 	blue,
-	bold,
 	dim,
 	green,
 	magenta,
@@ -16,6 +15,7 @@ type BenchmarkEndpoint = string | {
 	python: string;
 	go: string;
 };
+
 interface BenchmarkOptions {
 	/** The method of the `fetch` request */
 	method?: string;
@@ -27,6 +27,7 @@ interface BenchmarkOptions {
 	/** Whether to expect an error code intentionally (such as 401s) */
 	expectError?: number;
 }
+
 interface BenchmarkResults {
 	endpoint: BenchmarkEndpoint;
 	denoTime: number;
@@ -34,8 +35,24 @@ interface BenchmarkResults {
 	goTime: number;
 }
 
-/** Time a request */
-const timeRequest = async (url: string, options: BenchmarkOptions) => {
+/**
+ * Calculate the average of an array of numbers
+ *
+ * @param arr The array of data
+ * @returns The average
+ */
+const avg = (arr: number[]): number => {
+	return arr.reduce((prev, cur) => (prev + cur)) / arr.length;
+};
+
+/**
+ * Times a request
+ *
+ * @param url The URL to fetch
+ * @param options Benchmarking options
+ * @returns The response time
+ */
+const timeRequest = async (url: string | URL, options: BenchmarkOptions) => {
 	const alpha = performance.now();
 	const res = await fetch(url, {
 		body: options.body,
@@ -57,7 +74,7 @@ const timeRequest = async (url: string, options: BenchmarkOptions) => {
 const ITERATIONS = 10;
 const PROGRESS_BAR_WIDTH = 30;
 
-/** Progress bar */
+/** Write a progress bar to the console */
 const barFactory = async (
 	opts: { url: string; i: number; type: "deno" | "python" | "go" },
 ) => {
@@ -91,31 +108,49 @@ const barFactory = async (
 	await Deno.stdout.write(new TextEncoder().encode(bar));
 };
 
-/** Repeat an async function for many times and take the average of the numbers */
+/**
+ * Measure the average response time of an endpoint
+ *
+ * @param endpoint The endpoint along with the type of server being benchmarked
+ * @param options Benchmarking options
+ * @returns The average response time of the endpoint
+ */
 const measure = async (
-	info: { url: string; type: "deno" | "python" | "go" },
-	promiseFactory: () => Promise<number>,
+	endpoint: { url: string; type: "deno" | "python" | "go" },
+	options: BenchmarkOptions,
 ) => {
 	let i = 0;
 
 	const values: number[] = [];
 
-	await barFactory({ url: info.url, i, type: info.type });
+	await barFactory({ url: endpoint.url, i, type: endpoint.type });
 
 	for (i = 1; i <= ITERATIONS; i++) {
-		values.push(await promiseFactory());
+		values.push(
+			await timeRequest(
+				new URL(
+					endpoint.url,
+					endpoint.type === "deno"
+						? DENO_ROOT
+						: endpoint.type === "go"
+						? GO_ROOT
+						: PYTHON_ROOT,
+				),
+				options,
+			),
+		);
 
-		await barFactory({ url: info.url, i, type: info.type });
+		await barFactory({ url: endpoint.url, i, type: endpoint.type });
 	}
 
-	return values.reduce((prev, cur) => (prev + cur)) / values.length;
+	return avg(values);
 };
 
 /**
- * Benchmark an endpoint
+ * Measure an endpoint on all servers being benchmarked
  *
- * @param endpoint The absolute endpoint (can provide different endpoints for Deno and python)
- * @param options Options to provide for benchmarking
+ * @param endpoint The absolute endpoint (can provide different endpoints for different servers)
+ * @param options Benchmarking options
  */
 const benchmark = async (
 	endpoint: BenchmarkEndpoint,
@@ -135,31 +170,19 @@ const benchmark = async (
 
 	const denoTime = await measure(
 		{ url: denoEndpoint, type: "deno" },
-		() => timeRequest(DENO_ROOT + denoEndpoint, options),
+		options,
 	);
 	const pythonTime = await measure({
 		url: pythonEndpoint,
 		type: "python",
-	}, () =>
-		timeRequest(
-			PYTHON_ROOT + pythonEndpoint,
-			options,
-		));
+	}, options);
 	const goTime = await measure({
 		url: goEndpoint,
 		type: "go",
-	}, () =>
-		timeRequest(
-			GO_ROOT + pythonEndpoint,
-			options,
-		));
+	}, options);
 
 	return { endpoint, denoTime, pythonTime, goTime };
 };
-
-/**
- * THE BENCHMARKS
- */
 
 const basicAuthEndpoint: BenchmarkEndpoint = {
 	deno: "/auth/basic/hello/world",
@@ -251,11 +274,10 @@ const denoSummary = results.map((k) => k.denoTime);
 const pythonSummary = results.map((k) => k.pythonTime);
 const goSummary = results.map((k) => k.goTime);
 
-console.group(blue(bold("Deno:")));
+console.group(blue("Deno"));
 console.log(
 	"average:",
-	(denoSummary.reduce((prev, cur) => (prev + cur)) /
-		denoSummary.length).toFixed(2) + "ms",
+	avg(denoSummary).toFixed(2) + "ms",
 );
 console.log(
 	"min:",
@@ -267,11 +289,10 @@ console.log(
 );
 console.groupEnd();
 
-console.group(green(bold("Python:")));
+console.group(green("Python"));
 console.log(
 	"average:",
-	(pythonSummary.reduce((prev, cur) => (prev + cur)) /
-		pythonSummary.length).toFixed(2) + "ms",
+	avg(pythonSummary).toFixed(2) + "ms",
 );
 console.log(
 	"min:",
@@ -283,11 +304,10 @@ console.log(
 );
 console.groupEnd();
 
-console.group(magenta(bold("Go:")));
+console.group(magenta("Go"));
 console.log(
 	"average:",
-	(goSummary.reduce((prev, cur) => (prev + cur)) /
-		goSummary.length).toFixed(2) + "ms",
+	avg(goSummary).toFixed(2) + "ms",
 );
 console.log(
 	"min:",
@@ -298,3 +318,10 @@ console.log(
 	Math.max(...goSummary).toFixed(2) + "ms",
 );
 console.groupEnd();
+
+await Deno.writeTextFile(
+	"benchmark.results.json",
+	JSON.stringify(results, undefined, "	"),
+);
+
+console.log(dim("Saved data to benchmark.results.json"));
